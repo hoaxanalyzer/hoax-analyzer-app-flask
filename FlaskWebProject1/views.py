b2f23826c14e9c7dc9c18204f4a21dedd2a31d25
Adinda Budi <adinda.budi@gmail.com>
2017-03-26 18:18:18 +0700

    update views.py

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -6,7 +6,6 @@ from flask import render_template, request
 from FlaskWebProject1 import app
 import requests,json
 
-
 @app.route('/')
 def home():
     """Renders the home page."""
@@ -20,18 +19,17 @@ def result(id):
     data = {'id':id}
     r = requests.post(url, data=json.dumps(data), headers=headers)
     result =  r.json()
-    return json.dumps(result)
-    # fact_p = result["scores"][1]
-    # hoax_p = result["scores"][2]
-    # conclude = result["conclusion"]
-    # unkn_p = result["scores"][3]
-    # totalp = fact_p + hoax_p + unkn_p
-    # conclusion = "We cannot conclude anything. It's"
-    # if conclude == 'hoax':
-    #     conclusion = str(round(100*(hoax_p/totalp),2)) + "% searches said it's"
-    # elif conclude == 'fact' and fact_p!=0:
-    #     conclusion = str(round(100*(fact_p/totalp),2))  + "% searches said it's"
-    # return render_template('result.html', result=result, conclusion=conclusion)
+    fact_p = result["scores"][1]
+    hoax_p = result["scores"][2]
+    conclude = result["conclusion"]
+    unkn_p = result["scores"][3]
+    totalp = fact_p + hoax_p + unkn_p
+    conclusion = "We cannot conclude anything. It's"
+    if conclude == 'hoax':
+        conclusion = str(round(100*(hoax_p/totalp),2)) + "% searches said it's"
+    elif conclude == 'fact' and fact_p!=0:
+        conclusion = str(round(100*(fact_p/totalp),2))  + "% searches said it's"
+    return render_template('result.html', result=result, conclusion=conclusion)
 
 @app.route('/feedback/result', methods=['POST'])
 def feedbackResult():
