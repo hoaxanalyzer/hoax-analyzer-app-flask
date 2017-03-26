3f059d7d95cf2a1a035c540df53402354ca78b04
Adinda Budi <adinda.budi@gmail.com>
2017-03-26 12:35:52 +0700

    add deployment script

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -21,47 +21,47 @@ def result(id):
     r = requests.post(url, data=json.dumps(data), headers=headers)
     result =  r.json()
     return json.dumps(result)
-#     fact_p = result["scores"][1]
-#     hoax_p = result["scores"][2]
-#     conclude = result["conclusion"]
-#     unkn_p = result["scores"][3]
-#     totalp = fact_p + hoax_p + unkn_p
-#     conclusion = "We cannot conclude anything. It's"
-#     if conclude == 'hoax':
-#         conclusion = str(round(100*(hoax_p/totalp),2)) + "% searches said it's"
-#     elif conclude == 'fact' and fact_p!=0:
-#         conclusion = str(round(100*(fact_p/totalp),2))  + "% searches said it's"
-#     return render_template('result.html', result=result, conclusion=conclusion)
+    fact_p = result["scores"][1]
+    hoax_p = result["scores"][2]
+    conclude = result["conclusion"]
+    unkn_p = result["scores"][3]
+    totalp = fact_p + hoax_p + unkn_p
+    conclusion = "We cannot conclude anything. It's"
+    if conclude == 'hoax':
+        conclusion = str(round(100*(hoax_p/totalp),2)) + "% searches said it's"
+    elif conclude == 'fact' and fact_p!=0:
+        conclusion = str(round(100*(fact_p/totalp),2))  + "% searches said it's"
+    return render_template('result.html', result=result, conclusion=conclusion)
 
-# @app.route('/feedback/result', methods=['POST'])
-# def feedbackResult():
-#     if request.method == 'POST':
-#         data = request.json
-#         data["ip"] = _get_user_ip(request)
-#         data["browser"] = request.headers.get('User-Agent')
-#         url = 'https://ah.lelah.ga/feedback/result'
-#         headers = {'Content-Type':'application/json'}
-#         print data
-#         r = requests.post(url, data=json.dumps(data), headers=headers)
-#         result = r.json()
-#         return json.dumps(result)
+@app.route('/feedback/result', methods=['POST'])
+def feedbackResult():
+    if request.method == 'POST':
+        data = request.json
+        data["ip"] = _get_user_ip(request)
+        data["browser"] = request.headers.get('User-Agent')
+        url = 'https://ah.lelah.ga/feedback/result'
+        headers = {'Content-Type':'application/json'}
+        print data
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        result = r.json()
+        return json.dumps(result)
 
-# @app.route('/feedback/reference', methods=['POST'])
-# def feedbackReference():
-#     if request.method == 'POST':
-#         data = request.json
-#         data["ip"] = _get_user_ip(request)
-#         data["browser"] = request.headers.get('User-Agent')
-#         url = 'https://ah.lelah.ga/feedback/reference'
-#         headers = {'Content-Type':'application/json'}
-#         print data
-#         r = requests.post(url, data=json.dumps(data), headers=headers)
-#         result = r.json()
-#         return json.dumps(result)
+@app.route('/feedback/reference', methods=['POST'])
+def feedbackReference():
+    if request.method == 'POST':
+        data = request.json
+        data["ip"] = _get_user_ip(request)
+        data["browser"] = request.headers.get('User-Agent')
+        url = 'https://ah.lelah.ga/feedback/reference'
+        headers = {'Content-Type':'application/json'}
+        print data
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        result = r.json()
+        return json.dumps(result)
 
-# def _get_user_ip(request):
-#     ip = request.headers.get('X-Forwarded-For')
-#     if ip is None:
-#         ip = request.remote_addr
+def _get_user_ip(request):
+    ip = request.headers.get('X-Forwarded-For')
+    if ip is None:
+        ip = request.remote_addr
     
-#     return ip
+    return ip
