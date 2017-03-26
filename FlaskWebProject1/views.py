c82bca759d423a8bf472e23b76aa43fbcafa2cbc
Adinda Budi <adinda.budi@gmail.com>
2017-03-26 18:38:15 +0700

    update debugging

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -32,7 +32,7 @@ def result(id):
             conclusion = str(round(100*(fact_p/totalp),2))  + "% searches said it's"
         return render_template('result.html', result=result, conclusion=conclusion)
     except Exception as e:
-        return e
+        return render_template('result.html', conclusion = json.dumps(e))
 
 @app.route('/feedback/result', methods=['POST'])
 def feedbackResult():
