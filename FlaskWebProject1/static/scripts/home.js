4528fc799d91d4cf14c961126137cee91edc11d8
Asanilta Fahda <asanilta@gmail.com>
2017-04-03 13:36:28 +0700

    Menambahkan image uploader, bantuan, dan skema cara kerja di index

--- FlaskWebProject1/static/scripts/home.js
+++ FlaskWebProject1/static/scripts/home.js
@@ -1,3 +1,41 @@
+function uploadImage(){
+  var fileInput = $('#image-upload');
+  var maxSize = fileInput.data('max-size');
+  if(fileInput.get(0).files.length){
+      file = fileInput.get(0).files[0];
+      if (file) {
+        if (!file.name.match(/.(jpg|jpeg|png|gif|tif|tiff|bmp)$/i)) {
+          alert('Jenis file harus berupa JPEG, PNG, GIF, TIF, atau BMP');
+          return false;
+        } else {
+          var fileSize = file.size; // in bytes
+          if (fileSize>maxSize){
+            alert('Ukuran file tidak boleh lebih besar dari ' + maxSize/(1024*1024) + ' MB');
+            return false;
+          } else {
+            var img = new Image();
+            img.src = window.URL.createObjectURL( file );
+            img.onload = function() {
+              var width = img.naturalWidth,
+              height = img.naturalHeight;
+              window.URL.revokeObjectURL( img.src );
+              if (width<40 || height<40 || width>3200 || height>3200) {
+                alert('Dimensi gambar harus antara 40x40 dan 3200x3200 pixels');
+                return false;
+              } else {
+                alert('Gambar berhasil divalidasi');
+              }
+            }
+          }
+        }
+      } else {
+        return false;
+      }
+  } else {
+    return false;
+  }
+}
+
 function isInputValid(){
   var words = $("#hoax-analyzer.ha-main-input").val().split(" ");
 
@@ -15,8 +53,8 @@ function toStepTwo() {
     $("#ha-loader").fadeIn().css("display", "block");
     $("#ha-research").fadeIn().css("display", "block");
 
-	  query = $("#hoax-analyzer.ha-main-input").val();
-	  callAnalyzerAPI(query);
+    query = $("#hoax-analyzer.ha-main-input").val();
+    callAnalyzerAPI(query);
   }else{
     alert("Masukan tidak benar. Harus memiliki 3 kata atau lebih. Harap coba kembali.");
   }
@@ -35,5 +73,5 @@ function callAnalyzerAPI(q) {
     failure: function(errMsg) {
         alert(errMsg);
     }
-	});
+  });
 }
